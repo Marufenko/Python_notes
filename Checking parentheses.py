19fcb9ac5d5054374bd9b2c0af022e3c5579b9be
Marufenko <price.of.the.flight@hotmail.com>
2016-07-22 00:53:05 +0300

    Add files via upload

---
+++ Checking parentheses.py
@@ -0,0 +1,18 @@
+def function(L):
+    char_list = []
+    
+    for char in L:
+        if char in [')',']','}'] and len(char_list) == 0:
+            return False
+        elif char == '(':
+            char_list.append(')')
+        elif char == '[':
+            char_list.append(']')
+        elif char == '{':
+            char_list.append('}')
+        elif char == char_list[-1]:
+            char_list.pop()
+        elif char != char_list[-1]:
+            return False
+        print "char: " + char + " char_list: " + str(char_list)
+    return True
