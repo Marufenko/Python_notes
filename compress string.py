827aa77afd540dfa55256f28df059ef8e1c2bf2e
Marufenko <price.of.the.flight@hotmail.com>
2016-08-02 10:17:29 +0300

    Add files via upload

---
+++ compress string.py
@@ -0,0 +1,15 @@
+def compress(string):
+    '''compress row from "aaaaaaaaabbc" to "a9b2c1"'''
+    dictionary = {}
+    result = ''
+
+    for char in string:
+        if char not in dictionary.keys():
+            dictionary[char] = 1
+        else:
+            dictionary[char] = dictionary[char] + 1
+
+    for key in dictionary.keys():
+        result += str(key) + str(dictionary[key])
+
+    return result
