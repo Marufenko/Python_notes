cdcdffa5261f6d7bf235256107a3d8509829f737
Marufenko <price.of.the.flight@hotmail.com>
2016-08-18 23:14:33 +0300

    Update most common word.py

--- most common word.py
+++ most common word.py
@@ -1,20 +1,19 @@
-import re
-
 name = raw_input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
+if len(name) < 1 : name = "1.txt"
 handle = open(name)
+text = handle.read()
+words = text.split()
 
-pairs = {}
-for row in handle:
-    row = row.strip()
-    if row.startswith("From "):
-        pairs[row.split()[1]] = pairs.get(row.split()[1], 0) + 1
-        
+counts = {}
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+
 max_v = None
 min_k = None
-for k,v in pairs.items():
+for k,v in counts.items():
     if max_v is None or v > max_v:
         max_v = v
         max_k = k
 
 print max_k, max_v
+
