77f15954d97b651e11351f6e190660c6a440a9a8
Marufenko <price.of.the.flight@hotmail.com>
2016-08-22 23:17:13 +0300

    Update parse_html_BeatifulSoup.py

--- parse_html_BeatifulSoup.py
+++ parse_html_BeatifulSoup.py
@@ -1,12 +1,17 @@
 import urllib
 from bs4 import BeautifulSoup
 
-html = urllib.urlopen('http://python-data.dr-chuck.net/comments_42.html').read() #open url
+url = raw_input('Enter - ')
+
+html = urllib.urlopen(url).read() #open url
 soup = BeautifulSoup(html) #read url by BeautifulSoup
 
 count = 0
+sum = 0
 tags = soup('span') # Retrieve all of the anchor tags
 for tag in tags:
-    count += int(tag.contents[0]) # count sum of contant of all tags <span>
+    count += 1
+    sum += int(tag.contents[0]) # count sum of contant of all tags <span>
 
-print count
+print 'Count ' + str(count)
+print 'Sum ' + str(sum)
