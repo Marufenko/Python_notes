94ff3e8f751fc497e55c8b031062de1a889a11ae
Marufenko <price.of.the.flight@hotmail.com>
2016-09-03 18:11:24 +0300

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,16 @@
+# 1
+numbers = [1,2,3,4,5]
+number = [number for number in numbers if number < 4]
+
+# 2
+if my_object:
+    ... # if my_object is not empty -> True
+
+# 3
+if substring in string # check in string
+
+# 4
+# print list
+a = ['a', 'b', 'c']
+print 'Some %s.' %, ', '.join(a)
+# it returns "Some a, b, c."
