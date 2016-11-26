4cdd4aafca6787696c57a40395fb5862207d0774
mmarufenko <mmarufenko@hotmail.com>
2016-11-26 01:07:08 +0100

    add integral calculator

---
+++ integral.py
@@ -0,0 +1,25 @@
+import re
+
+def integral(leftBorder,rightBorder,step):
+    '''
+    function calculates intergal of 'function' on appropriate range with specified step of accuracy
+    leftBorder  - float
+    rightBorder - float
+    step        - float
+    '''
+    result = 0
+    point = leftBorder
+    while point < rightBorder:
+        x = point
+        result += float(calc(x)) * step
+        point += step
+
+    return result
+
+def calc(x):
+    '''
+    put here function
+    '''
+    return (2+((2.75*x)**3.2))**0.5
+
+print(integral(0,7,0.000005))
