fe487ea0e96dbbd9706f82e3b66c6fc5b4c83ae2
Marufenko Mykhailo <price.of.the.flight@hotmail.com>
2017-09-03 15:26:15 +0200

    let's try commit using ssh key

--- ascii_letters.py
+++ ascii_letters.py
@@ -5,3 +5,4 @@ UNTRUSTED_PREFIXES = tuple(["/", "\\"] + [c + ":" for c in string.ascii_letters]
 print(UNTRUSTED_PREFIXES)
 
 # let's try to add something by autocommit...
+# let's try commit using ssh key
