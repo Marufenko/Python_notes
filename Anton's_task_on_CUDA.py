ea91a1bccaedc39ac1b0d4660c24b539474b5aad
Marufenko Mykhailo <price.of.the.flight@hotmail.com>
2017-09-14 01:49:37 +0200

    Anton's task CPU vs GPU

--- Anton's_task_on_CUDA.py
+++ Anton's_task_on_CUDA.py
@@ -1,5 +1,9 @@
+import pycuda.gpuarray as gpuarray
+import pycuda.driver as cuda
+import pycuda.autoinit
 import numpy
 import time
+
 start_time = time.time()
 
 input_value = int(input('Input value: '))
@@ -10,13 +14,14 @@ for k in range(int(10**((len(str(input_value))/2)-1)), int(10**(len(str(input_va
     if len(str(int(input_value)/k)) == float_len:
         min_val = (int(input_value)/k)
 
-a_cpu = numpy.arange(int(min_val), int(10**(len(str(input_value))/2)), dtype=numpy.float)
-a_calc = input_value/a_cpu + a_cpu
+
+a_gpu = gpuarray.to_gpu(numpy.arange(int(min_val), int(10**(len(str(input_value))/2)), dtype=numpy.float))
+a_calc = (input_value/a_gpu + a_gpu).get()
 
 final_pairs = {}
 for i in range(len(a_calc)):
     if a_calc[i].is_integer():
-        final_pairs[a_calc[i]] = str(a_cpu[i]) + ' & ' + str(input_value/a_cpu[i])
+        final_pairs[a_calc[i]] = str(a_gpu[i]) + ' & ' + str(input_value/a_gpu[i])
 
 max_sum = 0
 for k, v in final_pairs.items():
@@ -26,6 +31,3 @@ for k, v in final_pairs.items():
 print(final_pairs[max_sum])
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
-# 3153661524
-# 389490263200027000
